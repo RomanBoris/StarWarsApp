8e1c7f48affc59d0325cb871a50b3cb3f4c2b4d3
Roman <krill.kron@yandex.ru>
2026-04-06 19:32:49 +0300

    add stateFlow

--- app/src/main/java/com/pobezhkin/starwars/presentation/home/StarHomeViewModel.kt
+++ app/src/main/java/com/pobezhkin/starwars/presentation/home/StarHomeViewModel.kt
@@ -1,7 +1,26 @@
 package com.pobezhkin.starwars.presentation.home
 
+import androidx.lifecycle.ViewModel
+import com.pobezhkin.starwars.domain.usecase.GetStarHeroesUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import javax.inject.Inject
 
 @HiltViewModel
-class StarHomeViewModel {
+class StarHomeViewModel @Inject constructor(
+    private val getStarHeroesUseCase: GetStarHeroesUseCase,
+) : ViewModel() {
+
+    private val _state = MutableStateFlow<StarHomeUiState>(StarHomeUiState.Loading)
+    val state: StateFlow<StarHomeUiState> = _state.asStateFlow()
+
+    init {
+
+
+    }
+
+
+
 }
