0afe064038eba6d1b5d0f816dc3cf2a276e137a3
Roman <krill.kron@yandex.ru>
2026-04-07 18:07:40 +0300

    add in reposImpl save in cash for HomeScreen

--- app/src/main/java/com/pobezhkin/starwars/data/repository/StarHeroesRepositoryImpl.kt
+++ app/src/main/java/com/pobezhkin/starwars/data/repository/StarHeroesRepositoryImpl.kt
@@ -1,16 +1,20 @@
 package com.pobezhkin.starwars.data.repository
 
 
+import com.pobezhkin.starwars.data.local.entity.dao.StarHeroDao
+import com.pobezhkin.starwars.data.mapper.toEntity
 import com.pobezhkin.starwars.data.mapper.toStarHeroes
 import com.pobezhkin.starwars.data.remote.StarApiService
 import com.pobezhkin.starwars.domain.model.StarHeroes
 import com.pobezhkin.starwars.domain.repository.StarHeroesRepository
 import com.pobezhkin.starwars.domain.util.Result
+import kotlinx.coroutines.flow.first
 import java.io.IOException
 import javax.inject.Inject
 import retrofit2.HttpException
 class StarHeroesRepositoryImpl @Inject constructor(
     private val starApiService: StarApiService,
+    private val starHeroDao: StarHeroDao
 ) : StarHeroesRepository {
 
     override suspend fun getAllHeroes(): Result<List<StarHeroes>> {
@@ -18,9 +22,16 @@ class StarHeroesRepositoryImpl @Inject constructor(
             val starResponce = starApiService.getTopHeroes(
                 page = 1
             ).resultsHeroes.map { it.toStarHeroes() }
+            starHeroDao.clearHeroes()
+            starHeroDao.insertStarHeroes( starResponce.map { it.toEntity() } )
             Result.Success(starResponce)
         } catch (e: IOException) {
-            Result.Error("Нет подключения к интернету")
+            val cashed = starHeroDao.getAllStarHeroes()
+                .first()
+                .map { it.toStarHeroes() }
+            if (cashed.isEmpty()) Result.Error("Нет подключения к интернету")
+            else Result.Success(cashed)
+
         } catch (e: HttpException) {
             Result.Error("Ошибка сервера: ${e.code()}")
         } catch (e: Exception) {
